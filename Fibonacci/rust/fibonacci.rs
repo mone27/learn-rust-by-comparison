4b7de80bdeb7f7b5a9b5544655ac62267b84dcfb
mone27 <mone.massaro@gmail.com>
2018-08-07 19:25:39 +0200

    improved fibonacci rust: only one match arm

--- Fibonacci/rust/fibonacci.rs
+++ Fibonacci/rust/fibonacci.rs
@@ -3,9 +3,8 @@ use std::io::Write;
 
 fn fibonacci(n: u32) -> u32 {
     match n {
-        0 => 1,
-        1 => 1,
-        _ => fibonacci(n - 1) + fibonacci(n - 2),
+        0 | 1 => 1,
+        __ => fibonacci(n - 1) + fibonacci(n - 2),
     }
 }
 
