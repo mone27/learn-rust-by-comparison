0848171282bc1e8e10aa7513d195ae6937c76472
Simone Massaro <simone@linux-xps.fritz.box>
2020-06-13 19:07:48 +0200

    added cargo to rust fibonacci and ranamed to english. Added gitignore for cargo

--- Fibonacci/fibonacci-rust/src/main.rs
+++ Fibonacci/fibonacci-rust/src/main.rs
@@ -11,14 +11,14 @@ fn fibonacci(n: u32) -> u32 {
 fn main() {
     let mut n = String::new();
 
-    print!("Calcolo n-esimo numero di Fibonacci: ");
+    print!("Calculating nth Fibonacci number: ");
     io::stdout().flush().unwrap();
 
-    io::stdin().read_line(&mut n).expect("errore nella lettura da stdin");
+    io::stdin().read_line(&mut n).expect("error in writing in stdin");
 
     let n: u32 = match n.trim().parse() {
-            Ok(num) => num,
-            Err(_) => panic!("errore di conversione"),
+            Ok(n) => n,
+            Err(_) => panic!("error in number conversion"),
     };
 
     println!("\nF({})={}", n, fibonacci(n));
